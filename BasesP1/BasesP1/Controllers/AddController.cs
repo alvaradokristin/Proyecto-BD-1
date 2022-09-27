d01a606eeb6fa8d3b781c649bed6a45195858ff1
Kristin Alvarado G <alvaradokristin@gmail.com>
2022-09-27 17:04:21 -0600

    Basic .NET pages created for UI design

---
+++ BasesP1/BasesP1/Controllers/AddController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasesP1.Controllers
+{
+    public class AddController : Controller
+    {
+        public IActionResult AddClient()
+        {
+            return View();
+        }
+
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+    }
+}
---
+++ BasesP1/BasesP1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BasesP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
