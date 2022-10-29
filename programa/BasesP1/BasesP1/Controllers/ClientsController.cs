38140f67c52a26bd20ce1d022a2cec082d486f51
AivyMR <amasisrivera@gmail.com>
2022-10-28 21:47:22 -0600

    Adding some client views

---
+++ programa/BasesP1/BasesP1/Controllers/ClientsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasesP1.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult AddClient()
+        {
+            return View();
+        }
+        public IActionResult ShowClients()
+        {
+            return View("Clients");
+        }
+        public IActionResult ShowClientsBySector()
+        {
+            return View("ClientsBySector");
+        }
+        public IActionResult ShowClientsByZone()
+        {
+            return View("ClientsByZone");
+        }
+    }
+}
