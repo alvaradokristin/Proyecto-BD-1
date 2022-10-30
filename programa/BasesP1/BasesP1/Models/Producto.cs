188a608b3e5c3774e725d087998e91e60c29e97b
Kristin Alvarado G <alvaradokristin@gmail.com>
2022-10-29 23:05:45 -0600

    Fix the connection string

--- programa/BasesP1/BasesP1/Models/Producto.cs
+++ programa/BasesP1/BasesP1/Models/Producto.cs
@@ -6,16 +6,28 @@ namespace BasesP1.Models
     {
         [Key]
         [Required]
+        [StringLength(10)]
         public string? Codigo { get; set; }
+
+        [Required]
+        [StringLength(12)]
         public string? Nombre { get; set; }
+
+        [Required]
+        [StringLength(1)]
         public string? Activo { get; set; }
+
+        [Required]
+        [StringLength(30)]
         public string? Descripcion { get; set; }
 
         [Display(Name = "Precio Estandar")]
         public float? PrecioEstandar { get; set; }
 
+        [Required]
+        [StringLength(10)]
         [Display(Name = "Codigo de Familia Producto")]
-        public string? Codigo_familia { get; set; }
+        public string? Codigo_Familia { get; set; }
 
     }
 }
