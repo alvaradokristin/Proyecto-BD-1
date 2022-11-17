e386950cf733a7d84192500fd3d72baf07179449
Kristin Alvarado G <alvaradokristin@gmail.com>
2022-11-16 18:48:01 -0600

    More reports created
    Reports created:
    - Top 10 de productos más vendidos
    - Top 10 de productos más cotizados
    - Cotizaciones y Ventas por departamento
    - Ventas y cotizaciones por mes, por año
    - Top 10 de clientes con mayores ventas

--- programa/BasesP1/BasesP1/Controllers/ReportsController.cs
+++ programa/BasesP1/BasesP1/Controllers/ReportsController.cs
@@ -1,6 +1,7 @@
 ﻿using BasesP1.Data;
 using BasesP1.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
 
 namespace BasesP1.Controllers
 {
@@ -57,9 +58,17 @@ namespace BasesP1.Controllers
                 case "cvpd":
                     dataModel = reportData.getBQuotesSellsByDept();
                     break;
-                case "other":
+                case "vcpma":
+                    dataModel = reportData.getBQuotesSellsByMonthYear();
+                    break;
+                case "vcpmavp":
                     // Code specific to PersonalPolicy
                     break;
+                case "vpspd":
+                    // Code specific to PersonalPolicy
+                    break;
+                case "vpzpd":
+                // Code specific to PersonalPolicy
                 case null:
                     // Code for "any-other-than" cases :)
                     break;
@@ -95,6 +104,125 @@ namespace BasesP1.Controllers
             }
 
             return View(dataModel);
+        }
+
+        public IActionResult LoadTableReport()
+        {
+            //Create a model that wll store several models to be use in View
+            dynamic model = new ExpandoObject();
+
+            model.Data = null;
+
+            return View("TableReport", model);
+        }
+
+        public List<String> getTableHeaders(string ReportType)
+        {
+            //Create an array of the headers that will be used in View table
+            List<String> tableHeaders = new List<String>();
+
+            switch (ReportType)
+            {
+                case "ttpmv":
+                    tableHeaders.Add("Codigo");
+                    tableHeaders.Add("Nombre");
+                    tableHeaders.Add("Activo");
+                    tableHeaders.Add("Descripcion");
+                    tableHeaders.Add("Precio");
+                    tableHeaders.Add("Codigo Familia");
+                    tableHeaders.Add("Ventas");
+                    break;
+                case "ttpmc":
+                    tableHeaders.Add("Codigo");
+                    tableHeaders.Add("Nombre");
+                    tableHeaders.Add("Activo");
+                    tableHeaders.Add("Descripcion");
+                    tableHeaders.Add("Precio");
+                    tableHeaders.Add("Codigo Familia");
+                    tableHeaders.Add("Cotizaciones");
+                    break;
+                case "ttccmv":
+                    tableHeaders.Add("Codigo");
+                    tableHeaders.Add("Nombre Cuenta");
+                    tableHeaders.Add("Telefono");
+                    tableHeaders.Add("Celular");
+                    tableHeaders.Add("Correo");
+                    tableHeaders.Add("Informacion Adicional");
+                    tableHeaders.Add("Asesor");
+                    tableHeaders.Add("Moneda");
+                    tableHeaders.Add("Sector");
+                    tableHeaders.Add("Sitio Web");
+                    tableHeaders.Add("Zona");
+                    tableHeaders.Add("Ventas");
+                    break;
+                case null:
+                    // Code for "any-other-than" cases :)
+                    break;
+            }
+
+            return tableHeaders;
+        }
+
+        public IActionResult TableReport(string ReportType)
+        {
+            //Create a model that wll store several models to be use in View
+            dynamic model = new ExpandoObject();
+
+            //Create an array of the headers that will be used in View table
+            List<String> tableHeaders = new List<String>();
+
+            //List of products with the values from the query
+            List<Product> product = new List<Product>();
+            List<Client> client = new List<Client>();
+
+            ProductData queryProd = new ProductData(this.Configuration);
+            ReportsData queryReport = new ReportsData(this.Configuration);
+
+            switch (ReportType)
+            {
+                case "ttpmv":
+                    // Get the table headers
+                    tableHeaders = getTableHeaders(ReportType);
+
+                    //Get the data from the query
+                    product = queryProd.getReport(1);
+
+                    //Add the data to the general model that cointains dirrefent models inside
+                    model.Headers = tableHeaders;
+                    model.Data = product;
+                    model.Type = ReportType;
+
+                    break;
+                case "ttpmc":
+                    // Get the table headers
+                    tableHeaders = getTableHeaders(ReportType);
+
+                    //Get the data from the query
+                    product = queryProd.getReport(3);
+
+                    //Add the data to the general model that cointains dirrefent models inside
+                    model.Headers = tableHeaders;
+                    model.Data = product;
+                    model.Type = ReportType;
+                    break;
+                case "ttccmv":
+                    // Get the table headers
+                    tableHeaders = getTableHeaders(ReportType);
+
+                    //Get the data from the query
+                    client = queryReport.getTClientMostSells();
+
+                    //Add the data to the general model that cointains dirrefent models inside
+                    model.Headers = tableHeaders;
+                    model.Data = client;
+                    model.Type = ReportType;
+                    break;
+                case null:
+                    // Default
+                    break;
+            }
+
+            return View(model);
         }
     }
 }
